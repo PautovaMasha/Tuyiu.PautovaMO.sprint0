ef0d6efb25f1f3a96893bdfc636454ebfcbd1230
PautovaMasha <@DESKTOP-6COMKTF>
2025-09-09 22:42:06 +0500

    Task2

---
+++ Tyuiu.PautovaMO.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PautovaMO.Sprint0.Task2.V0.Lib;
+
+
+namespace Tyuiu.PautovaMO.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessagValid()
+
+        {
+            var name = "Мария";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет..., Мария", res);
+
+        }
+    }
+}
